13d10c86354fa6bf790f2aba9e1545855707eb99
Aagya <creativitywithaagya@gmail.com>
2026-04-03 01:15:41 +0530

    my first upload

---
+++ addition calc.py
@@ -0,0 +1,4 @@
+print ("WELCOME TO ADDITION CACULATOR")
+a = int(input("enter the first number you want to add: "))
+b = int(input("enter the second number you want to add: "))
+print ("Therefore, the answer is", a+b)
