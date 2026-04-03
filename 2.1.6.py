84077d9c78fac7921297e9293ec38ea60ca92b39
Aagya Jain <creativitywithaagya@gmail.com>
2026-04-03 20:35:27 +0530

    Allow float input for square calculation
    Changed input type from int to float for square calculation.

--- 2.1.6.py
+++ 2.1.6.py
@@ -1,6 +1,6 @@
 # to find the square of the number entered by the user
 print ("WELCOME TO THE SQUARE CALCULATOR")
-a = int(input("enter the number you want the square of: "))
+a = float(input("enter the number you want the square of: "))
 print ("Therefore the square is", a*a)
 ''' ANOTHER WAY IS 
 print ("Therefore the square is", a**2)'''  
