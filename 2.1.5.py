2ca6ac113d67e22a27389112473238d99da08125
Aagya <creativitywithaagya@gmail.com>
2026-04-04 11:51:29 +0530

    Create 2.1.5.py

---
+++ 2.1.5.py
@@ -0,0 +1,5 @@
+# this is to remove average
+print ("WELCOME TO AVERAGE CALCULATOR")
+a = int(input("enter the first number: "))
+b = int(input("enter the second number: "))
+print ("Therefore, the average is " , (a+b)/2)
